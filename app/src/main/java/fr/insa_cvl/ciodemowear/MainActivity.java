034b9f7b25f16ce72443ccb7541b6ba3d1da2743
jf <jean-francois.lalande@irisa.fr>
2017-12-17 21:29:15 +0100

    Initial commit

---
+++ app/src/main/java/fr/insa_cvl/ciodemowear/MainActivity.java
@@ -0,0 +1,28 @@
+package fr.insa_cvl.ciodemowear;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends WearableActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.text);
+
+        // Enables Always-on
+        setAmbientEnabled();
+    }
+
+    public void button_click(View view) {
+        Intent i = new Intent(this, Activity2.class);
+        startActivity(i);
+    }
+}
